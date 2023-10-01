39e46b8edc1b8d299d788463970ca8040ec6a400
trYhKrzychu <grzegorzkoziol.gk@gmail.com>
2023-10-01 22:17:41 +0200

    fix 0.51

--- script.js
+++ script.js
@@ -1,63 +1,72 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-
-if (localStorage.getItem("scrollPosition")) {
-    window.scrollTo(0, parseInt(localStorage.getItem("scrollPosition")));
-}
-if (window.location.hash) {
-    history.replaceState({}, document.title, window.location.href.split('#')[0]);
-}
-window.addEventListener("scroll", function () {
-    localStorage.setItem("scrollPosition", window.scrollY);
+  handleLocalStorageScroll();
+  handleWindowLoad();
+  handleScrollEffects();
+  handleTippyTooltips();
 });
 
+function handleLocalStorageScroll() {
+  const scrollPosition = localStorage.getItem("scrollPosition");
+  if (scrollPosition) {
+      window.scrollTo(0, parseInt(scrollPosition));
+  }
 
+  if (window.location.hash) {
+      history.replaceState({}, document.title, window.location.href.split('#')[0]);
+  }
 
-    var loader  =   document.getElementById("loading");
-      window.addEventListener("load", function(){
-              loader.style.display = "none";
-      })
-
-
-
-      if (window.innerWidth > 850) {
-        ScrollReveal().reveal('.aboutme-main', { delay: 250 });
-        ScrollReveal().reveal('.made-web-window', { interval: 200 });
-        ScrollReveal().reveal('.services-examples-card', { delay: 250 });
-    }
-    
-
-const targetElement = document.querySelector('.made');
-const kolor1 = getComputedStyle(document.documentElement).getPropertyValue('--kolor1');
-const kolor9 = getComputedStyle(document.documentElement).getPropertyValue('--kolor9');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= targetElement.offsetTop) {
-        document.body.classList.add('change-background');
-        document.body.style.backgroundColor = kolor9;
-    } else {
-        document.body.classList.remove('change-background');
-        document.body.style.backgroundColor = kolor1;
-    }
-
-    if (window.scrollY >= targetElement.offsetTop + targetElement.clientHeight) {
-        document.body.style.backgroundColor = kolor1;
-    }
-});
-
-
-tippy('#made-web-windows-text-tippy', {
-    content: "Zobacz stronę!",
-    placement: 'bottom',
+  window.addEventListener("scroll", function () {
+      localStorage.setItem("scrollPosition", window.scrollY);
   });
+}
+
+function handleWindowLoad() {
+  const loader = document.getElementById("loading");
+  window.addEventListener("load", function () {
+      loader.style.display = "none";
+  });
+}
+
+function handleScrollEffects() {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth > 850) {
+      ScrollReveal().reveal('.aboutme-main', { delay: 250 });
+      ScrollReveal().reveal('.made-web-window', { interval: 200 });
+      ScrollReveal().reveal('.services-examples-card', { delay: 250 });
+  }
+
+  const targetElement = document.querySelector('.made');
+  const kolor1 = getComputedStyle(document.documentElement).getPropertyValue('--kolor1');
+  const kolor9 = getComputedStyle(document.documentElement).getPropertyValue('--kolor9');
+
+  window.addEventListener('scroll', () => {
+      if (window.scrollY >= targetElement.offsetTop) {
+          document.body.classList.add('change-background');
+          document.body.style.backgroundColor = kolor9;
+      } else {
+          document.body.classList.remove('change-background');
+          document.body.style.backgroundColor = kolor1;
+      }
+
+      if (window.scrollY >= targetElement.offsetTop + targetElement.clientHeight) {
+          document.body.style.backgroundColor = kolor1;
+      }
+  });
+}
+
+function handleTippyTooltips() {
+  const tippyOptions = {
+      placement: 'bottom',
+  };
+
+  tippy('#made-web-windows-text-tippy', {
+      content: "Zobacz stronę!",
+      placement: 'bottom',
+  });
+
   tippy('#tippy-footer', {
-    content: "Zapisz się",
-    placement: 'bottom',
+      content: "Zapisz się",
+      placement: 'bottom',
   });
-
-
-
-
-
-});
-
+}
